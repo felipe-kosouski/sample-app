76dd25499ac903926706a7a6524c03903abb98ce
Felipe Kosouski <felipe.kosouski@gmail.com>
2018-03-23 13:57:03 -0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  root 'static_pages#home'
+  get  'static_pages/home'
   get 'static_pages/help'
-
-  root 'application#hello'
+  get 'static_pages/about'
+  get 'static_pages/contact'
 end
