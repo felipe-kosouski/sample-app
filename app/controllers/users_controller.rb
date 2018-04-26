b708f5b824ff8dbc5979c04fc1de5242a6c53fcc
Felipe Kosouski <felipe.kosouski@gmail.com>
2018-04-26 20:31:14 -0300

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the sample App!"
       redirect_to @user
     else
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,10 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
 end
